1d38a501db45ca17979b00ed3864aefe6ff245c4
Rob Cruz <alekseicruz@gmail.com>
2020-02-15 13:27:56 -0800

    Initial checkin

--- app.js
+++ app.js
@@ -6,9 +6,25 @@ const proxyurl = "https://cors-anywhere.herokuapp.com/";
 const url = "https://stamps.testrail.io/index.php?/api/v2/get_case/4930"
 
 
-fetch(url, {
-  headers: {
-    "Authorization": "Basic " + username + ":" + apiKey,
-    "Content-Type": "application/json"
-  }
-}).then(res => console.log(res))
+// fetch(url, {
+//   headers: {
+//     "Authorization": "Basic cmNydXpAc3RhbXBzLmNvbTppVVBLSVU0UWVoZ1BQQWo5cGdQSy1uS1JpaFovVFNYdlphaE0uUUhBUw==",
+//     "Content-Type": "application/json"
+//   }
+// }).then(res => console.log(res))
+
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Basic cmNydXpAc3RhbXBzLmNvbTppVVBLSVU0UWVoZ1BQQWo5cGdQSy1uS1JpaFovVFNYdlphaE0uUUhBUw==");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://stamps.testrail.io/index.php?/api/v2/get_case/4930", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
