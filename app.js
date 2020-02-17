a6f20bbf0cdfbd1bb9bff3389da5379fe4b03982
Rob Cruz <alekseicruz@gmail.com>
2020-02-16 16:22:21 -0800

    Sorted out javascript errors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 
-let movieTitle = "12341234asdfasdfasffsd!"
+
+let movieTitle = "Avengers Endgame"
+let movieInfo = null
 
 var settings = {
   "async": true,
@@ -8,27 +10,58 @@ var settings = {
   "method": "GET"
 }
 
-$.ajax(settings)
-  .then(response => console.log(response))
-  .catch(err => console.log(err));
+testCallback(settings, getMovieInfo)
 
-
-
-
-settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": `https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=${movieTitle}`,
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
-    "x-rapidapi-key": "b1ca774955msh58c2e44d8019f1cp19dff8jsn096f4ba49c6a"
-  }
+function testCallback(settings, callback){
+  $.ajax(settings)
+    .then(callback)
+    .catch(err => console.log(err));
 }
 
-$.ajax(settings)
-  .then(response => console.log(response))
-  .catch(err => console.log(err));
+function getMovieInfo(response) {
+  alert('getMovieInfo')
+  movieInfo = response
+  alert(movieInfo['Title'])
+  console.log(movieInfo)
+}
 
 
 
+// var settings = {
+//   "async": true,
+//   "crossDomain": true,
+//   "url": `http://www.omdbapi.com/?apikey=c164b08f&t=${movieTitle}`,
+//   "method": "GET"
+// }
+//
+// testCallback(settings, getMovieInfo)
+//
+// function testCallback(settings, callback){
+//   $.ajax(settings)
+//     .then(callback)
+//     .catch(err => console.log(err));
+// }
+
+
+
+
+
+
+
+// settings = {
+//   "async": true,
+//   "crossDomain": true,
+//   "url": `https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=${movieTitle}`,
+//   "method": "GET",
+//   "headers": {
+//     "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+//     "x-rapidapi-key": "b1ca774955msh58c2e44d8019f1cp19dff8jsn096f4ba49c6a"
+//   }
+// }
+//
+// $.ajax(settings)
+//   .then(response => console.log(response))
+//   .catch(err => console.log(err));
+//
+//
+//
