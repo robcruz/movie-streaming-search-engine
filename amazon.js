9829c2e372cb10d7499c247184c2dfbbf18e07c4
Rob Cruz <alekseicruz@gmail.com>
2020-02-23 22:24:25 -0800

    Refactored console.log calls

--- amazon.js
+++ amazon.js
@@ -1,5 +1,8 @@
 
-let title = "Ford v Ferrari"
+// let title = "Captain America: The First Avenger"
+// let title = "Captain America: Civil War"
+let title = "BoJack Horseman"
+
 let response
 
 getOMDBMovie(title)
@@ -7,42 +10,51 @@ getOMDBMovie(title)
   .catch(rejectOMDBResponse)
 
 function getOMDBMovie(title) {
-  return $.ajax({
-    "async": true,
-    "crossDomain": true,
-    "url": `http://www.omdbapi.com/?apikey=c164b08f&t="${title}"`,
-    "dataType": "jsonp",
-    "method": "GET"
-  })
-}
-
-function resolveOMDBResponse(response){
-  console.log(response.imdbID)
-  debugger
-  // var settings = {
+  // return $.ajax({
   //   "async": true,
   //   "crossDomain": true,
-  //   "url": `https://streamzui-streamzui-v1.p.rapidapi.com/search?country=us&imdbid=${response.imdbID}&yearend=2019&yearstart=1900&page=1`,
-  //   "method": "GET",
+  //   "url": `http://www.omdbapi.com/?apikey=c164b08f&t="${title}"`,
   //   "dataType": "jsonp",
-  //   "headers": {
-  //     "x-rapidapi-host": "streamzui-streamzui-v1.p.rapidapi.com",
-  //     "x-rapidapi-key": "b1ca774955msh58c2e44d8019f1cp19dff8jsn096f4ba49c6a"
-  //   }
-  // }
+  //   "method": "GET"
+  // })
 
   var settings = {
     "async": true,
     "crossDomain": true,
-    "url": `https://streamzui-streamzui-v1.p.rapidapi.com/search?country=us&imdbid=${response.imdbID}&yearend=2019&yearstart=1900&page=1`,
+    "url": `https://imdb8.p.rapidapi.com/title/find?q=${title}`,
     "method": "GET",
     "headers": {
-      "x-rapidapi-host": "streamzui-streamzui-v1.p.rapidapi.com",
+      "x-rapidapi-host": "imdb8.p.rapidapi.com",
       "x-rapidapi-key": "b1ca774955msh58c2e44d8019f1cp19dff8jsn096f4ba49c6a"
     }
   }
 
-  console.log(settings)
+  return $.ajax(settings).done(function (response) {
+    console.log('getOMDBMovie')
+    console.log(response)
+  });
+
+}
+
+function getId(str) {
+  return str.match(/^\/title\/(.+)\/$/)[1]
+}
+
+function resolveOMDBResponse(response){
+
+  console.log(getId(response.results[0].id))
+  debugger
+
+  let settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": `https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?country=US&source_id=${getId(response.results[0].id)}&source=imdb`,
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+      "x-rapidapi-key": "b1ca774955msh58c2e44d8019f1cp19dff8jsn096f4ba49c6a"
+    }
+  }
 
   $.ajax(settings)
     .then(function (res) {
